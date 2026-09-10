50f6859155e34c4f81f91a8c35badaae4c8096c6
aksharachenchu <aksharachenchu64@gmail.com>
2026-09-10 21:56:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,12 +41,14 @@ ALLOWED_EXTENSIONS = {'csv', 'txt', 'xlsx'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-import os
-
-MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+def get_db():
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="seating_db"
+    )
+    return db
 
 # ============================================
 # ACCESS CONTROL HELPERS
